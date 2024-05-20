f272ab02b2f1fd34365f722c3526d1468d51422d
jitim3 <ivan.booc@gmail.com>
2024-05-21 02:54:01 +0800

    database

---
+++ src/project/ui/ViewReviewsAdmin.java
@@ -0,0 +1,90 @@
+package project.ui;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import javax.swing.JScrollBar;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ViewReviewsAdmin extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewReviewsAdmin frame = new ViewReviewsAdmin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	public ViewReviewsAdmin() {
+		setTitle("View Reviews");
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Dell\\Desktop\\clone\\project\\beach2.png"));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("VIEW REVIEWS");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 22));
+		lblNewLabel.setBounds(227, 25, 195, 39);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("EXIT");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				adminMain menu = new adminMain();
+				menu.show();
+				
+				dispose();
+			}
+		});
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+		btnNewButton.setBounds(287, 413, 85, 21);
+		btnNewButton.setFocusable(false);
+		contentPane.add(btnNewButton);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(644, 59, 17, 323);
+		contentPane.add(scrollBar);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setBounds(22, 59, 639, 323);
+		lblNewLabel_2.setOpaque(true);
+		lblNewLabel_2.setBackground(new Color (100, 255, 255, 64));		
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Dell\\Desktop\\clone\\project\\figma.jpg"));
+		lblNewLabel_1.setBounds(0, 0, 676, 453);
+		contentPane.add(lblNewLabel_1);
+		
+		
+		
+		
+		
+	
+	}
+}
---
+++ src/project/ui/adminMain.java
@@ -0,0 +1,105 @@
+package project.ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class adminMain extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					adminMain frame = new adminMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public adminMain() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Dell\\Desktop\\clone\\project\\beach2.png"));
+		setTitle("MENU");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 500);
+		contentPane = new JPanel();
+		contentPane.setForeground(new Color(0, 0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Edit Information");
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+		btnNewButton.setBounds(251, 53, 199, 46);
+		btnNewButton.setFocusable(false);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("View Reviews");
+		btnNewButton_1.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				ViewReviewsAdmin review = new ViewReviewsAdmin();
+				review.show();
+				
+				dispose();
+			}
+		});
+		btnNewButton_1.setBounds(251, 119, 199, 46);
+		btnNewButton_1.setFocusable(false);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("View Reservation");
+		btnNewButton_2.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+		btnNewButton_2.setBounds(251, 192, 199, 46);
+		btnNewButton_2.setFocusable(false);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("View Wallet");
+		btnNewButton_3.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+		btnNewButton_3.setBounds(251, 264, 199, 46);
+		btnNewButton_3.setFocusable(false);
+		contentPane.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("View Transaction");
+		btnNewButton_4.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+		btnNewButton_4.setBounds(251, 334, 199, 46);
+		btnNewButton_4.setFocusable(false);
+		contentPane.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("EXIT");
+		btnNewButton_5.setFont(new Font("Times New Roman", Font.PLAIN, 10));
+		btnNewButton_5.setBounds(309, 414, 85, 21);
+		btnNewButton_5.setFocusable(false);
+		contentPane.add(btnNewButton_5);
+		
+		JLabel lblNewLabel = new JLabel(".");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Dell\\Desktop\\clone\\project\\figma.jpg"));
+		lblNewLabel.setBounds(0, 0, 686, 463);
+		contentPane.add(lblNewLabel);
+	}
+}
