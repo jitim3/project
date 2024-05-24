b47ffbdadf27663576be1082692e7e9103973a71
Julian Jupiter <julianjupiter.io@gmail.com>
2024-05-24 10:14:18 +0800

    Update SQL

--- src/project/ui/ViewReviewsAdmin.java
+++ src/project/ui/ViewReviewsAdmin.java
@@ -7,16 +7,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
-public class ViewReviewsAdmin implements ActionListener {
+import project.dto.ReviewDto;
+import project.service.ReviewService;
+import project.service.impl.DefaultReviewService;
 
+public class ViewReviewsAdmin implements ActionListener {
+	private final ReviewService reviewService;
 	JFrame frame = new JFrame("View Reviews");
 	JButton btnEditInfo = new JButton("Edit Information");
 	JScrollBar scrollBar = new JScrollBar();
@@ -25,9 +33,24 @@ public class ViewReviewsAdmin implements ActionListener {
 	JButton btnExit = new JButton("EXIT");
 	private final JFrame parentFrame;
 
-	public ViewReviewsAdmin(JFrame parentFrame) {
+	public ViewReviewsAdmin(JFrame parentFrame, long resortId) {
 		this.parentFrame = parentFrame;
-
+		this.reviewService = new DefaultReviewService();
+		
+		List<ReviewDto> reviewDtos = this.reviewService.getReviewsByResortId(resortId);
+		String reviews = reviewDtos.stream()
+				.map(reviewDto -> """
+						User ID: %s
+						Rate:    %d
+						Comment: %s
+						""".formatted(reviewDto.userId(), reviewDto.rate(), reviewDto.comment()))
+				.peek(System.out::println)
+				.collect(Collectors.joining("\n\n"));
+		JTextArea textArea = new JTextArea(reviews); 
+        textArea.setWrapStyleWord(true); 
+        textArea.setLineWrap(true); 
+        JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); 		  
+        scrollBar = scrollPane.getVerticalScrollBar(); 
 		scrollBar.setBounds(629, 64, 17, 267);
 
 		btnExit.setFont(new Font("Times New Roman", Font.PLAIN, 10));
